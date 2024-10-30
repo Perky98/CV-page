5c19d618fc8c3913f29553fe766c0bfe1483427c
qwerty982003 <qwerty982003@gmail.com>
2024-10-30 19:15:56 +0000

    Chybičky

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,19 @@ function App() {
         <h1>Juraj Hertel</h1>
         <h2>Student & Developer</h2>
         <div>
+
           <a href="https://github.com/Perky98" target="_blank">
             <img src={github} className="small-logo" alt="Github" />
           </a>
+
           <a href="mailto:jurajhertel98@gmail.com">
             <img src={mail} className="small-logo" alt="Maik" />
           </a>
+
           <a href="https://www.instagram.com/juraj_hertel/" target="_blank">
             <img src={instagram} className="small-logo" alt="Instagram" />
           </a>
+          
         </div>
       </div>
     </section>
